c25e1c87333649f5763e85fa21dbbe7145cf0e26
swizel03 <leesandra.aus@gmail.com>
2020-08-26 14:51:20 +1000

    Added first iteration of Profile Page

---
+++ app/src/main/java/com/example/utselibrary/ProfilePage.java
@@ -0,0 +1,72 @@
+package com.example.utselibrary;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class ProfilePage extends AppCompatActivity {
+    FirebaseAuth fAuth = FirebaseAuth.getInstance();
+    FirebaseFirestore fStore = FirebaseFirestore.getInstance();
+    String userID = fAuth.getCurrentUser().getUid();
+    DocumentReference documentReference = fStore.collection("Users").document(userID);
+    TextView firstNameTf, lastNameTf, mobileNoTf, uniIdTf, totalFinesTf;
+    Button changeDetailsBtn, changePassBtn, backBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_page2);
+        firstNameTf = findViewById(R.id.firstNameTf);
+        lastNameTf = findViewById(R.id.lastNameTf);
+        mobileNoTf = findViewById(R.id.mobileNoTf);
+        uniIdTf = findViewById(R.id.uniIdTf);
+        totalFinesTf = findViewById(R.id.totalFinesTf);
+        backBtn = findViewById(R.id.backBtn);
+
+        documentReference.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                if (documentSnapshot.exists()) {
+                    String fiName = documentSnapshot.getString("firstName");
+                    String laName = documentSnapshot.getString("lastName");
+                    String mo = documentSnapshot.getString("phoneNumber");
+                    String unId = documentSnapshot.getString("uniID");
+
+                    firstNameTf.setText(fiName); // Display first name
+                    lastNameTf.setText(laName); // Display last name
+                    mobileNoTf.setText(mo); // Display mobile number
+                    uniIdTf.setText(unId); // Display uni ID
+                } else {
+                    Toast.makeText(ProfilePage.this, "Error: Details cannot be displayed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(ProfilePage.this, "Error: Details cannot be displayed", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Dashboard.class));
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/utselibrary/UserLogin.java
+++ app/src/main/java/com/example/utselibrary/UserLogin.java
@@ -1,8 +1,5 @@
 package com.example.utselibrary;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -11,6 +8,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -73,6 +73,7 @@ public class UserLogin extends AppCompatActivity {
                                         startActivity(new Intent(getApplicationContext(), Dashboard.class));
                                         progressBar.setVisibility(View.INVISIBLE);
                                         userLoginBtn.setVisibility(View.VISIBLE);
+                                        startActivity(new Intent(getApplicationContext(), Dashboard.class));
                                     } else {
                                         Toast.makeText(UserLogin.this, "Invalid account", Toast.LENGTH_SHORT).show();
                                         FirebaseAuth.getInstance().signOut();
