2a2de07efb0a1fc5f1a18a63641ea4afdcc12be1
swizel03 <leesandra.aus@gmail.com>
2020-08-26 14:48:21 +1000

    Added first iteration of Profile Page

--- app/src/main/java/com/example/utselibrary/Dashboard.java
+++ app/src/main/java/com/example/utselibrary/Dashboard.java
@@ -1,14 +1,27 @@
 package com.example.utselibrary;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class Dashboard extends AppCompatActivity {
+
+    Button profileBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard2);
+        profileBtn = findViewById(R.id.profileBtn);
+
+        profileBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), ProfilePage.class));
+            }
+        });
     }
 }
