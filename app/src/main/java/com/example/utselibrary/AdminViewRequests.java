109a528353a5a130d46474cb00c2c55b513440d8
Homer Aaron Martillano <hamartillano@iron-man-2.home>
2020-09-28 14:14:33 +1000

    Commit Message: - Created first iteration of book request form and view book requests (admin). Back-end code TBC for both.

---
+++ app/src/main/java/com/example/utselibrary/AdminViewRequests.java
@@ -0,0 +1,14 @@
+package com.example.utselibrary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminViewRequests extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_view_requests);
+    }
+}
