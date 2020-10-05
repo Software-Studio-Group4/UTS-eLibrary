6de7527ebc9925c38570be66807581f50d974d90
oattions <angelasunn98@gmail.com>
2020-10-05 21:07:42 +1100

    borrowing books now works. It doesn't show up on the user interface, but the information is there on the database
    KNOWN BUG :
    You can borrow the same book twice.

--- app/src/main/java/com/example/utselibrary/Model/Documents.java
+++ app/src/main/java/com/example/utselibrary/Model/Documents.java
@@ -1,62 +1,35 @@
 
 package com.example.utselibrary.Model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Documents {
 
     private String title;
     private String primaryAuthor;
     private String genre;
     private String id;
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-
-    private String imageUrl;
-
-    public String getPublisher() {
-        return publisher;
-    }
-
-    public void setPublisher(String publisher) {
-        this.publisher = publisher;
-    }
-
     private String publisher;
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-
-
-    public String getGenre() {
-        return genre;
-    }
-
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-
+    private String imageUrl;
+    private int borrowLimit;
+    private List<String> borrowers = new ArrayList<String>();
 
     public Documents() {
         // Empty constructor
     }
 
-    public Documents(String title, String primaryAuthor, String id, String genre, String publisher, String imageUrl) {
+    public Documents(String title, String primaryAuthor, String id, String genre, String publisher, String imageUrl, int borrowLimit, String[] borrowers) {
         this.title = title;
         this.primaryAuthor = primaryAuthor;
         this.id = id;
         this.genre = genre;
         this.publisher = publisher;
         this.imageUrl = imageUrl;
+        this.borrowLimit = borrowLimit;
+        for(int i = 0; i < borrowers.length; i++) {
+            this.borrowers.add(borrowers[i]);
+        }
     }
 
     public String getTitle() {
@@ -71,6 +44,57 @@ public class Documents {
         return primaryAuthor;
     }
 
+    public void setPrimaryAuthor(String primaryAuthor) {
+        this.primaryAuthor = primaryAuthor;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public int getBorrowLimit() {
+        return borrowLimit;
+    }
+
+    public void setBorrowLimit(int borrowLimit) {
+        this.borrowLimit = borrowLimit;
+    }
+
+    public List<String> getBorrowers() {
+        return borrowers;
+    }
+
+    public void setBorrowers(List<String> borrowers) {
+        this.borrowers = borrowers;
+    }
 
 }
 
